a161860c352eb7e2c6d81fde9292653c3b02e669
J K <jack2727272000@hotmail.com>
2019-11-24 22:29:40 +0800

    靜香~現在10:28分了，我好累唷...

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("HelloWorld");
             Console.WriteLine("靜香在我家~我...不在家");
             Console.WriteLine("靜香~我5點後才會在家!!!!");
+            Console.WriteLine("靜香~現在10:28分了，我好累唷...");
             Console.ReadLine();
         }
     }
